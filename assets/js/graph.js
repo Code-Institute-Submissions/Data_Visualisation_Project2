582fe0ee8ff6523a380a519da763a8159be853f2
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2019-04-08 18:10:26 +0100

    updated d3 scale linear

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -5,14 +5,23 @@ queue()
 function makeGraphs(error, worldData){
   var ndx = crossfilter(worldData);
 
+  worldData.forEach(function(d){
+    d.GDP = parseInt(d.GDP);
+    d.LifeExpectancy = parseInt(d.LifeExpectancy);
+  })
+
+  // I have to parse the data above for the scatter plots to render on my graph. If i dont parse in a string to integer(number) the dotts sit at the bottom of the graph
+
   show_country_selector(ndx);
   show_status_balance(ndx);
   show_average_life_expectancy(ndx);
   show_average_mortality_ofAdults(ndx);
   show_correlation_lifeLongeativity_gdp(ndx);
+  
   dc.renderAll();
 }
 
+// SelectMenu
 function show_country_selector(ndx){
   var countryDim = ndx.dimension( function (v){ return v.Country;
   });
@@ -26,7 +35,13 @@ function show_country_selector(ndx){
     })
   }
 
+// Pie Chart
 function show_status_balance(ndx){
+
+  var color_country = d3.scale.ordinal()
+  .domain(["Developed", "Developing"])
+  .range(["red", "black"]);
+
   var statusDim = ndx.dimension(
     function (v){
       return v.Status;
@@ -39,14 +54,32 @@ function show_status_balance(ndx){
   .group(statusGroup)
   .cx(195)
   .cy(175)
+  .colors(color_country)
   .drawPaths(true)
   .innerRadius(60)
   .radius(150)
-  .transitionDuration(750);
+  .transitionDuration(750)
+  // .attr("fill", function (d) {
+  //          if (d == "Developed") {
+  //              return "purple";
+  //          } else if (d == "Developing") {
+  //              return "black";
+  //          } else {
+  //              return "red"; // misc. to test if none above work
+  //          }
+  //      });
 
+ // !!!! IMPORTANT -  why doesnt the above code work?
 }
 
+// Bar Graph
+
 function show_average_life_expectancy(ndx){
+
+  var color_country = d3.scale.ordinal()
+  .domain(["Developed", "Developing"])
+  .range(["red", "black"]);
+
   var statusDimension = ndx.dimension(dc.pluck("Status"));
   var average_life_age = statusDimension.group().reduce(
     function (p, v){
@@ -73,6 +106,10 @@ function show_average_life_expectancy(ndx){
   dc.barChart("#average_life_expectancy")
   .width(350)
   .height(350)
+  .colors(color_country)
+  .colorAccessor(function(d) {
+    return d.key[0];
+  })
   .margins({top: 10, right: 50, bottom: 30, left: 50})
   .dimension(statusDimension)
   .group(average_life_age)
@@ -93,7 +130,14 @@ function show_average_life_expectancy(ndx){
 
 }
 
+// Bar Chart
+
 function show_average_mortality_ofAdults(ndx) {
+
+  var color_country = d3.scale.ordinal()
+  .domain(["Developed", "Developing"])
+  .range(["red", "black"]);
+
   var mortalityDim = ndx.dimension(dc.pluck("Status"));
   var percentage_adultDeath = mortalityDim.group().reduce(
     function (p, v) {
@@ -121,6 +165,7 @@ function show_average_mortality_ofAdults(ndx) {
   dc.barChart("#average_adult_mortality")
     .width(350)
     .height(350)
+    .colors(color_country)
     .margins({ top: 10, right: 50, bottom: 30, left: 50})
     .dimension(mortalityDim)
     .group(percentage_adultDeath)
@@ -145,11 +190,14 @@ function show_correlation_lifeLongeativity_gdp(ndx){
   var color_country = d3.scale.ordinal()
   .domain(["Developed", "Developing"])
   .range(["red", "black"]);
-  // color_country("Developed"); red
-  // color_country("Developing"); black
+  
   var gdpDim = ndx.dimension(dc.pluck("GDP"));
-  var gdp_age = ndx.dimension(function(d) {
-    return [d.GDP, d.Country, d.Status];
+  //var gdpDim = ndx.dimension(function(d) {
+    //return parseInt(d.GDP);
+  //})
+
+  var gdp_age = ndx.dimension(function (d) {
+    return [d.GDP, d.LifeExpectancy, d.Country, d.Status];// The positioning of what goes here is important, first one (gdp) is x axis and second one lifeExp is y axis 
   });
   var age_gdp_correlation = gdp_age.group();
 
@@ -157,28 +205,28 @@ function show_correlation_lifeLongeativity_gdp(ndx){
   var maxGdp = gdpDim.top(1)[0].GDP;
 
   dc.scatterPlot("#gdp_lifeExpectancy")
-    .width(800)
-    .height(400)
-    .x(d3.scale.linear().domain([minGdp, maxGdp]))
+    .width(900)
+    .height(500)
+    .x(d3.scale.linear().domain([0, 90000])) // you can even put your own numbers here instead of min/max values
+    .y(d3.scale.linear().domain([50, 90]))
     .brushOn(false)
     .symbolSize(8)
     .clipPadding(10)
     .yAxisLabel("Life Expectancy")
     .xAxisLabel("GDP")
     .title(function(d) {
-      return d.key[1];
+      return d.key[2];
     })
-    .colorAccessor(function(d) {
-      return d.key[3];
-    })
-    .colors(color_country)
-    .dimension(gdp_age)
+    .dimension(gdpDim)
     .group(age_gdp_correlation)
-    .margins({top: 10, right: 50, bottom: 75, left: 75})
+    .colors(color_country)
+    .colorAccessor(function(d) {
+       return d.key[3];
+    })
+    .margins({top: 70, right: 50, bottom: 75, left: 75})
+    .transitionDuration(750);
 
 }
-
-
 
 
 
